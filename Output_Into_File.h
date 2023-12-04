d47e55261571fe8ea11b8de5bb05a73b4232e334
iuleu <139135460+iuleu@users.noreply.github.com>
2023-12-04 22:11:58 +0300

    модуль вывода

---
+++ Output_Into_File.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <io.h>
+#include <limits>
+#include <vector>
+#include "Enum_Filework.h"
+
+void Output_Into_File(std::vector<int>& vect, std::vector<int>& vect2);
